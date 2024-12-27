cdbba46b2ab7c86e234b308a08878a701ff73e7c
GoFDD <juliocesargolfredo2022@gmail.com>
2024-12-27 12:50:19 -0300

    feat: add structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,38 @@
-import Image from "next/image";
+import { Header } from "./components/header/header";
+import "./styles/page.scss";
 
 export default function Home() {
   return (
     <div>
-      <main>
-        <Image
-          aria-hidden
-          src="/file.svg"
-          alt="File icon"
-          width={16}
-          height={16}
-        />
+      <main className="container">
+        <Header />
+        <div className="experience">
+          <h3>Experience</h3>
+          <p>
+            Atualmente, possuo experiências em projetos acadêmicos realizados em
+            parceria com empresas locais de Marília, como Fabrica de Códigos e
+            Eficaz Marketing.
+          </p>
+          <div className="experience-time"></div>
+          <div className="infos">
+            <h3>Languages</h3>
+            <div className="languages-info">
+              <span>🇺🇸 EN - Basic</span>
+              <span>🇧🇷 PT-BR - Native Speaker</span>
+            </div>
+            <h3>Education</h3>
+            <div className="educational-info">
+              <span>🎓</span>
+              <span>
+                Analise e Desenvolvimento de Sistemas - UNIMAR, Marilia
+              </span>
+            </div>
+          </div>
+          <div className="buttons">
+            <div className="social"></div>
+            <button>contact me</button>
+          </div>
+        </div>
       </main>
       <footer></footer>
     </div>
